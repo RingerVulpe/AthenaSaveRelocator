5be2cb62a7828fdcb62d389717eaeffc5f832acd
A. Therrien <ringervulpe@gmail.com>
2025-01-22 02:24:28 -0800

    will not let the application run if already running

--- Program.cs
+++ Program.cs
@@ -8,7 +8,13 @@ namespace AthenaSaveRelocator
         [STAThread]
         static void Main()
         {
-            // EXACT same functionality as before:
+            // Prevent multiple instances of the application
+            if (System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("Another instance of the application is already running.", "Athena Save Relocator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new AthenaSaveRelocatorForm());
